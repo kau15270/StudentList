40bacd5c39dd0ed7420e1912f2fb9577d222c41c
Sehajpreet Kaur <kau15270@sheridancollege.ca>
2023-05-16 09:58:26 -0400

    hlo

---
+++ src/Student.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class Student {
+   private String sname;
+   private String sID;
+
+    public Student(String sname, String sID) {
+        this.sname = sname;
+        this.sID = sID;
+    }
+   
+   
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public void setsID(String sID) {
+        this.sID = sID;
+    }
+   
+
+    public String getSname() {
+        return sname;
+    }
+
+    public String getsID() {
+        return sID;
+    }
+   
+}
---
+++ src/student/list/StudentList.java
@@ -0,0 +1,23 @@
+
+package student.list;
+
+import java.util.Scanner;
+
+public class StudentList 
+
+    
+    public static void main(String[] args) {
+        
+        Student s1=new Student(sname:"Sehaj",sID:"s1");
+        Scanner input=new Scanner(source:System.in);
+        Student[] studentList=new Student[3];
+        studentList[0]=s1;
+        studentList[1]=new Student(sname:"gurpriyal",sID:"s2");
+        studentList[2]=new Student(sname:"ishika",sID:"S3");
+        
+        for(int i=0;i<studentList.length;i++){
+            System.out.println(x:studentList[i].getSname());
+        }
+    }
+    
+}
